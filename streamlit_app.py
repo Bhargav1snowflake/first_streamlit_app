cc8851a9abda9ad5cb28175ecb4b9cb84e86ca76
Bhargav1snowflake <121058438+Bhargav1snowflake@users.noreply.github.com>
2022-12-20 11:10:25 +0530

    Update streamlit_app.py
    added images_correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My parents New Healthy Dinner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text(:bowl_with_spoon: 'omega 3 &Blueberry Oatmeal')
-streamlit.text(:leafy_green: 'Kale, Spinach & Rocket Smoothie')
-streamlit.text(	:chicken: 'HArd-Boiled Free-Range Egg')
-streamlit.text	:avocado::sandwich: ' avocado & sandwich')
+streamlit.text(':bowl_with_spoon: omega 3 &Blueberry Oatmeal')
+streamlit.text(':leafy_green: Kale, Spinach & Rocket Smoothie')
+streamlit.text(	':chicken: HArd-Boiled Free-Range Egg')
+streamlit.text(':avocado::sandwich: avocado & sandwich')
